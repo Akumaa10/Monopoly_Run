743b3cba6035b56173ddd67bb1047634c80803a2
RKY-L <143527631+RKY-L@users.noreply.github.com>
2025-11-08 22:16:00 -0500

    NavBar changing to a different board

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import pygame
-import Player
-pygame.init()
-SCREEN_W, SCREEN_H = 400, 719
-screen = pygame.display.set_mode((SCREEN_W, SCREEN_H))
+import pygame_gui
 
+from Player import Player
+
+from buffs import render_buff
+from board import render_board
+
+pygame.init()
+SCREEN_W, SCREEN_H = 324, 582
+screen = pygame.display.set_mode((SCREEN_W, SCREEN_H))
+manager = pygame_gui.UIManager((SCREEN_W, SCREEN_H))
 
 screen.fill((255, 255, 255))
 clock = pygame.time.Clock()
@@ -11,11 +17,32 @@ clock = pygame.time.Clock()
 running = True
 
 player = Player()
+button_render = render_buff(screen) #default render
+
+def nav_bar_manager(page):
+    global button_render
+    if(page == "board"):
+        print("rendering the board")
+        button_render = render_board(screen)
+    elif(page == "buffs"):
+        button_render = render_buff(screen)
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            render_result = button_render(screen,event.pos)
+            if(render_result):
+                nav_bar_manager(render_result)
+
+        manager.process_events
+
+    #Renders screen aswell as ui elements
+    time_delta = clock.tick(30) / 1000.0
+    manager.update(time_delta)
+    manager.draw_ui(screen)
     pygame.display.flip()
-    clock.tick(30) #fps limiter
+
 
 pygame.quit()
